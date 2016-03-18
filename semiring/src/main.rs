b3642b3cb23179649cd91ddc67436112b997727d
Daniel van Niekerk <dvn.demitasse@gmail.com>
2016-03-18 20:21:24 +0200

    implemented basic tropical weight for floating point types... better way possible?

--- semiring/src/main.rs
+++ semiring/src/main.rs
@@ -6,13 +6,13 @@ use semiring::TropicalWeight;
 fn main() {
     {
         let t = TropicalWeight::new(Some(12f64));
-        let tt = TropicalWeight::new(Some(13f64));
+        let tt = TropicalWeight::one();
         println!("{}", tt.is_member());
         println!("{:?} ⊕ {:?} = {:?}", t, tt, t.plus(tt)); 
         println!("{:?} ⊗ {:?} = {:?}", t, tt, t.times(tt)); 
     }
     let t = TropicalWeight::new(Some(10.0f32));
-    let tt = TropicalWeight::new(Some(13.0f32));
+    let tt = TropicalWeight::zero();
     println!("{}", tt.is_member());
     println!("{:?} ⊕ {:?} = {:?}", t, tt, t.plus(tt)); 
     println!("{:?} ⊗ {:?} = {:?}", t, tt, t.times(tt)); 
--- semiring/src/semiring.rs
+++ semiring/src/semiring.rs
@@ -1,31 +1,31 @@
 extern crate num;
-use num::Float;
+use std::{f64, f32};
 use std::option::Option;
 
 pub trait Weight {
     fn is_member(&self) -> bool;
     fn plus(self, rhs: Self) -> Self;
     fn times(self, rhs: Self) -> Self;
-    // fn zero() -> Self;
-    // fn one() -> Self;
+    fn zero() -> Self;
+    fn one() -> Self;
 }
 
 //We give the struct "Copy semantics", i.e. it will be copied instead
 // of "moved". We may want to revisit this decision, but in the
 // meantime this is more in line with the behaviour of numeric types.
 #[derive(Copy, Clone, Debug)]
-pub struct TropicalWeight<T: Float> {
+pub struct TropicalWeight<T> {
     val: Option<T>
 }
 
-impl<T: Float> TropicalWeight<T> {
+impl<T> TropicalWeight<T> {
     pub fn new(val: Option<T>) -> TropicalWeight<T> {
         TropicalWeight {val: val}
     }
 }
 
-impl<T: Float> Weight for TropicalWeight<T> {
-    fn plus(self, rhs: TropicalWeight<T>) -> TropicalWeight<T> {
+impl Weight for TropicalWeight<f64> {
+    fn plus(self, rhs: TropicalWeight<f64>) -> TropicalWeight<f64> {
         if (!self.is_member()) || (!rhs.is_member()) {
             TropicalWeight::new(None)
         } else if self.val < rhs.val {
@@ -35,7 +35,7 @@ impl<T: Float> Weight for TropicalWeight<T> {
         }        
     }
 
-    fn times(self, rhs: TropicalWeight<T>) -> TropicalWeight<T> {
+    fn times(self, rhs: TropicalWeight<f64>) -> TropicalWeight<f64> {
         if (!self.is_member()) || (!rhs.is_member()) {
             TropicalWeight::new(None)
         } else {
@@ -43,22 +43,55 @@ impl<T: Float> Weight for TropicalWeight<T> {
         }
     }
 
+    fn zero() -> TropicalWeight<f64> {
+        TropicalWeight::new(Some(f64::INFINITY))
+    }
+
+    fn one() -> TropicalWeight<f64> {
+        TropicalWeight::new(Some(0.0))
+    }
+
     fn is_member(&self) -> bool {
-        //DEMIT: Revisit? -- rejects NaN but not negative infinity (as in openFST)
-        self.val == self.val 
+        if let Some(val) = self.val {
+            !(val == f64::NAN || val == f64::NEG_INFINITY)
+        } else {
+            false
+        }
     }
 }
 
-// //ZERO
-// impl<T: Zero + Add<Output=T> + PartialOrd + Clone> Zero for TropicalWeight<T> {
-//     fn zero() -> TropicalWeight<T> {
-//         TropicalWeight::new(Some(T::infinity()))
-//     }
-// }
+impl Weight for TropicalWeight<f32> {
+    fn plus(self, rhs: TropicalWeight<f32>) -> TropicalWeight<f32> {
+        if (!self.is_member()) || (!rhs.is_member()) {
+            TropicalWeight::new(None)
+        } else if self.val < rhs.val {
+            TropicalWeight::new(self.val)
+        } else {
+            TropicalWeight::new(rhs.val)
+        }        
+    }
 
-// //ONE
-// impl<T: Zero + Add<Output=T> + PartialOrd + Clone> One for TropicalWeight<T> {
-//     fn one() -> TropicalWeight<T> {
-//         TropicalWeight::new(Some(T::zero()))
-//     }
-// }
+    fn times(self, rhs: TropicalWeight<f32>) -> TropicalWeight<f32> {
+        if (!self.is_member()) || (!rhs.is_member()) {
+            TropicalWeight::new(None)
+        } else {
+            TropicalWeight::new(Some(self.val.unwrap() + rhs.val.unwrap()))
+        }
+    }
+
+    fn zero() -> TropicalWeight<f32> {
+        TropicalWeight::new(Some(f32::INFINITY))
+    }
+
+    fn one() -> TropicalWeight<f32> {
+        TropicalWeight::new(Some(0.0))
+    }
+
+    fn is_member(&self) -> bool {
+        if let Some(val) = self.val {
+            !(val == f32::NAN || val == f32::NEG_INFINITY)
+        } else {
+            false
+        }
+    }
+}
