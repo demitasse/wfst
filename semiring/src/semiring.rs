27aebf2b6e014d52c5803e9801b06b818ce31cbe
Daniel van Niekerk <dvn.demitasse@gmail.com>
2016-03-26 22:25:47 +0200

    ported LogWeight and refactored value unwraps...

--- semiring/src/semiring.rs
+++ semiring/src/semiring.rs
@@ -19,6 +19,7 @@ pub trait Float<T>: Copy + PartialOrd + Add<Output=T> + Sub<Output=T> {
     fn nan() -> T;
     fn infty() -> T;
     fn neg_infty() -> T;
+    fn logexp(self) -> T;
     fn approx_eq(self, rhs: T, delta: Option<f32>) -> bool;
     fn quantize(self, delta: Option<f32>) -> T;
 }
@@ -44,6 +45,10 @@ impl Float<f64> for f64 {
         f64::NEG_INFINITY
     }
 
+    fn logexp(self) -> f64 {
+        (1.0 + (-self).exp()).ln()
+    }
+
     fn approx_eq(self, rhs: f64, delta: Option<f32>) -> bool {
         let d = if let Some(d) = delta {
             d as f64
@@ -61,11 +66,12 @@ impl Float<f64> for f64 {
         };
         if self == f64::NEG_INFINITY ||
             self == f64::INFINITY ||
-            self == f64::NAN {
-                self
-            } else {
-                (self / d + 0.5).floor() * d
-            }
+            self == f64::NAN
+        {
+            self
+        } else {
+            (self / d + 0.5).floor() * d
+        }
     }
 }
 
@@ -90,6 +96,10 @@ impl Float<f32> for f32 {
         f32::NEG_INFINITY
     }
 
+    fn logexp(self) -> f32 {
+        (1.0 + (-self).exp()).ln()
+    }
+
     fn approx_eq(self, rhs: f32, delta: Option<f32>) -> bool {
         let d = if let Some(d) = delta {
             d
@@ -107,11 +117,12 @@ impl Float<f32> for f32 {
         };
         if self == f32::NEG_INFINITY ||
             self == f32::INFINITY ||
-            self == f32::NAN {
-                self
-            } else {
-                (self / d + 0.5).floor() * d
-            }
+            self == f32::NAN
+        {
+            self
+        } else {
+            (self / d + 0.5).floor() * d
+        }
     }
 }
 
@@ -147,10 +158,10 @@ pub enum DivideType {
 ////////////////////////////////////////////////////////////////////////////////
 // Weight and thus also semiring implementations
 
-// We most of these structs "Copy semantics", i.e. it will be copied
-// instead of "moved". We may want to revisit this decision, but in
-// the meantime this is more in line with the behaviour of numeric
-// types.
+// We give most of these structs "Copy semantics", i.e. it will be
+// copied instead of "moved". We may want to revisit this decision,
+// but in the meantime this is more in line with the behaviour of
+// numeric types.
 
 ////////////////////////////////////////////////////////////////////////////////
 //TROPICAL SEMIRING: (min, +, inf, 0)
@@ -170,9 +181,9 @@ impl<T: Float<T>> Weight<TropicalWeight<T>> for TropicalWeight<T> {
         if (!self.is_member()) || (!rhs.is_member()) {
             TropicalWeight::new(None)
         } else if self.val < rhs.val {
-            TropicalWeight::new(self.val)
+            self   //because TropicalWeight is Copy
         } else {
-            TropicalWeight::new(rhs.val)
+            rhs    //because TropicalWeight is Copy
         }        
     }
 
@@ -180,7 +191,8 @@ impl<T: Float<T>> Weight<TropicalWeight<T>> for TropicalWeight<T> {
         if (!self.is_member()) || (!rhs.is_member()) {
             TropicalWeight::new(None)
         } else {
-            TropicalWeight::new(Some(self.val.unwrap() + rhs.val.unwrap()))
+            let (v1, v2) = (self.val.unwrap(), rhs.val.unwrap());
+            TropicalWeight::new(Some(v1 + v2))
         }
     }
 
@@ -223,12 +235,15 @@ impl<T: Float<T>> Weight<TropicalWeight<T>> for TropicalWeight<T> {
     fn divide(self, rhs: TropicalWeight<T>, divtype: Option<DivideType>) -> TropicalWeight<T> {
         if (!self.is_member()) || (!rhs.is_member()) {
             TropicalWeight::new(None)
-        } else if rhs.val.unwrap() == T::infty() {
-            TropicalWeight::new(None)
-        } else if self.val.unwrap() == T::infty() {
-            TropicalWeight::new(Some(T::infty()))
         } else {
-            TropicalWeight::new(Some(self.val.unwrap() - rhs.val.unwrap()))
+            let (v1, v2) = (self.val.unwrap(), rhs.val.unwrap());
+            if v2 == T::infty() {
+                TropicalWeight::new(None)
+            } else if v1 == T::infty() {
+                self   //because TropicalWeight is Copy
+            } else {
+                TropicalWeight::new(Some(v1 - v2))
+            }
         }
     }
 
@@ -243,3 +258,102 @@ impl<T: Float<T>> Weight<TropicalWeight<T>> for TropicalWeight<T> {
 
 ////////////////////////////////////////////////////////////////////////////////
 //LOG SEMIRING: (log(e^-x + e^y), +, inf, 0)
+#[derive(Copy, Clone, Debug, Hash, RustcEncodable, RustcDecodable)]
+pub struct LogWeight<T: Float<T>> {
+    val: Option<T>
+}
+
+impl<T: Float<T>> LogWeight<T> {
+    pub fn new(val: Option<T>) -> LogWeight<T> {
+        LogWeight {val: val}
+    }
+}
+
+impl<T: Float<T>> Weight<LogWeight<T>> for LogWeight<T> {
+    fn plus(self, rhs: LogWeight<T>) -> LogWeight<T> {
+        let (v1, v2) = (self.val.unwrap_or(T::nan()), rhs.val.unwrap_or(T::nan()));
+        if v1 == T::infty() {
+            rhs   //because LogWeight is Copy
+        } else if v2 == T::infty() {
+            self  //because LogWeight is Copy
+        } else if v1 > v2 {
+            LogWeight::new(Some(v2 - (v1 - v2).logexp()))
+        } else {
+            LogWeight::new(Some(v1 - (v2 - v1).logexp()))
+        }
+    }
+
+    fn times(self, rhs: LogWeight<T>) -> LogWeight<T> {
+        if (!self.is_member()) || (!rhs.is_member()) {
+            LogWeight::new(None)
+        } else {
+            let (v1, v2) = (self.val.unwrap(), rhs.val.unwrap());
+            if v1 == T::infty() {
+                self   //because LogWeight is Copy
+            } else if v2 == T::infty() {
+                rhs    //because LogWeight is Copy
+            } else {
+                LogWeight::new(Some(v1 + v2))
+            }
+        }
+    }
+
+    fn zero() -> LogWeight<T> {
+        LogWeight::new(Some(T::infty()))
+    }
+
+    fn one() -> LogWeight<T> {
+        LogWeight::new(Some(T::zero()))
+    }
+
+    fn is_member(&self) -> bool {
+        if let Some(val) = self.val {
+            !(val == T::nan() || val == T::neg_infty())
+        } else {
+            false
+        }
+    }
+
+    fn approx_eq(self, rhs: Self, delta: Option<f32>) -> bool {
+        if let Some(val) = self.val {
+            if let Some(val2) = rhs.val {
+                val.approx_eq(val2, delta)
+            } else {
+                false
+            }
+        } else {
+            false
+        }        
+    }
+
+    fn quantize(self, delta: Option<f32>) -> LogWeight<T> {
+        if let Some(val) = self.val {
+            LogWeight::new(Some(val.quantize(delta)))    
+        } else {
+            LogWeight::new(None)
+        }
+    }
+
+    fn divide(self, rhs: LogWeight<T>, divtype: Option<DivideType>) -> LogWeight<T> {
+        if (!self.is_member()) || (!rhs.is_member()) {
+            LogWeight::new(None)
+        } else {
+            let (v1, v2) = (self.val.unwrap(), rhs.val.unwrap());
+            if v2 == T::infty() {
+                LogWeight::new(None)
+            } else if v1 == T::infty() {
+                self   //because LogWeight is Copy
+            } else {
+                LogWeight::new(Some(v1 - v2))
+            }
+        }
+    }
+
+    fn reverse(self) -> LogWeight<T> {
+        self
+    }
+
+    fn properties() -> u64 {
+        SEMIRING | COMMUTATIVE
+    }
+}
