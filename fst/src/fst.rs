bb2d5a9a5a15a639b8c17fdca41b188de4252646
Daniel van Niekerk <dvn.demitasse@gmail.com>
2016-05-26 00:01:15 +0200

    still reading the openfst code...

--- fst/src/fst.rs
+++ fst/src/fst.rs
@@ -24,27 +24,3 @@ impl<T: Weight> Arc<T> {
     }
 }
 
-// pub type Std32Arc = Arc<TropicalWeight<f32>>;
-// pub type Std64Arc = Arc<TropicalWeight<f64>>;
-// pub type Log32Arc = Arc<LogWeight<f32>>;
-// pub type Log64Arc = Arc<LogWeight<f64>>;
-// pub type Minmax32Arc = Arc<MinmaxWeight<f32>>;
-// pub type Minmax64Arc = Arc<MinmaxWeight<f64>>;
-
-// impl Std64Arc {
-//     pub fn new(i: Label, o: Label, w: Option<f64>, s: StateId) -> Std64Arc {
-//         Arc { ilabel: i,
-//               olabel: o,
-//               weight: TropicalWeight::<f64>::new(w),
-//               nextstate: s }
-//     }
-// }
-
-// impl Std32Arc {
-//     pub fn new(i: Label, o: Label, w: Option<f32>, s: StateId) -> Std32Arc {
-//         Arc { ilabel: i,
-//               olabel: o,
-//               weight: TropicalWeight::<f32>::new(w),
-//               nextstate: s }
-//     }
-// }
